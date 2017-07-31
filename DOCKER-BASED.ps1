6dc866d34a581b2510a1a0168060e2e429ac1b7a
Joe Tauke <jt5055@iowanetworkservices.net>
2017-07-31 15:42:11 -0500

    Completed script.  Added recursion sed query, as well as bind restart.

--- DOCKER-BASED.ps1
+++ DOCKER-BASED.ps1
@@ -13,9 +13,6 @@ $NetworkSubnetBits = "/24"
 #Create Docker host on our ESXI server
 docker-machine create -d vmwarevsphere --vmwarevsphere-vcenter $EsxiHost --vmwarevsphere-username $EsxiUsername --vmwarevsphere-password $EsxiPassword --vmwarevsphere-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v17.06.1-ce-rc2/boot2docker.iso $DockerHostName
 
-#Setup SSH to container Host
-docker-machine env $DockerHostName | Invoke-Expression
-
 #Set IP addresses to match clients DNS servers
 docker-machine ssh $DockerHostName sudo ip addr add $DockerHostPrimaryIP$NetworkSubnetBits dev eth0
 docker-machine ssh $DockerHostName sudo ip addr add $DockerHostSecondaryIP$NetworkSubnetBits dev eth0
@@ -28,10 +25,12 @@ $SecondaryIPSet = "ip addr add $DockerHostSecondaryIP$NetworkSubnetBits dev eth0
 docker-machine ssh $DockerHostName docker pull sameersbn/bind:latest
 
 #Start BIND Container
-docker-machine ssh $DockerHostName docker run -d --name=bind --dns=127.0.0.1 --publish=$DockerHostPrimaryIP':53:53/udp' --publish=$DockerHostSecondaryIP':53:53/udp' --publish=$DockerHostPrimaryIP':10000:10000' --volume=/srv/docker/bind:/data --env='ROOT_PASSWORD=@11ianc3' sameersbn/bind:latest
+docker-machine ssh $DockerHostName sudo docker run -d --name=bind --restart always --dns=127.0.0.1 --publish=$DockerHostPrimaryIP':53:53/udp' --publish=$DockerHostSecondaryIP':53:53/udp' --publish=$DockerHostPrimaryIP':10000:10000' --volume=/var/lib/docker/bind:/data --env='ROOT_PASSWORD=@11ianc3' sameersbn/bind:latest
 
 #Create persistant boot script to keep IP and start our container
 docker-machine ssh $DockerHostName sudo touch "/var/lib/boot2docker/bootsync.sh"
 docker-machine ssh $DockerHostName sudo chmod +x "/var/lib/boot2docker/bootsync.sh"
-docker-machine ssh $DockerHostName "echo -e '#! /bin/sh \n $PrimaryIPSet \n $SecondaryIPSet \n Docker start bind' | sudo tee -a /var/lib/boot2docker/bootlocal.sh"
+docker-machine ssh $DockerHostName docker exec bind sed -i '/listen-on-v6/a\allow-recursion\ {any\;}\;' /etc/bind/named.conf.options
+docker-machine ssh $DockerHostName docker exec bind service bind9 restart
+docker-machine ssh $DockerHostName "echo -e '#! /bin/sh \n $PrimaryIPSet \n $SecondaryIPSet' | sudo tee -a /var/lib/boot2docker/bootlocal.sh"
 
